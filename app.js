3fa2f6b1cfd32c4f73c2b176041e0d9559ee308a
Henry Nguyen <nguyenhai1591@gmail.com>
2020-05-06 22:07:29 +1000

    Test deploy

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get('/about', (req, res) => {
 // Mang routes vao
 app.use('/posts', posts)
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server khoi dong tai port ${PORT}`))
